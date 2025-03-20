1ad8a778c013d240e722490fe19265f5576d3c9c
a21sahfa <a21sahfa@student.his.se>
2025-03-20 02:32:11 +0100

    mongodb connected
    anslutade mongoDB till min VsCODE

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/databas.js";
+
+import authRouter from "./router/auth.router.js";
+
+dotenv.config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use ("/api/auth", authRouter);
+
+app.listen (PORT, () => {
+    console.log("server is running on PORT:" + PORT);
+    connectDB();
+});
---
+++ backend/src/kontroller/auth.kontroller.js
@@ -0,0 +1,11 @@
+export const register = (req, res) => {
+    res.send("register router");
+};
+
+export const login = (req, res) => {
+    res.send("login router");
+};
+
+export const logout = (req, res) => {
+    res.send("logout router");
+};
